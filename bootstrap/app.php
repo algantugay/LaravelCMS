0dabbc173957bc9e8130e72540484637b280d41a
Tugay Algan <algantugay@gmail.com>
2025-01-03 13:16:20 +0300

    app dosyası düzeltme

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,20 +1,23 @@
 <?php
 
+use App\Http\Middleware\LoginControl;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+
+return Application::configure(basePath: dirname(_DIR_))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: _DIR_ . '/../routes/web.php',
+        commands: _DIR_ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
+            'user' => LoginControl::class,
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })->create();
