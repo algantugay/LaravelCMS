f81695a52c233e9645737c011cd2edd310af1b24
Tugay Algan <algantugay@gmail.com>
2025-01-03 12:56:13 +0300

    çakışma düzeltme

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'user' => LoginControl::class,
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
