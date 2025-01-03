c030f9e29015df8c281be27585a75e2d50f9fdd5
Tugay Algan <algantugay@gmail.com>
2025-01-03 12:22:33 +0300

    User.php çakışması

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -19,15 +20,19 @@ class User extends Authenticatable
         'last_login'
     ];
 
-    public function role()
-    {
-        return $this->belongsTo(Role::class, 'role_id');
-    }
-
     protected $hidden = [
         'password',
         'remember_token',
     ];
+
+
+
+
+    
+    public function role()
+    {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
 
     protected function casts(): array
     {
